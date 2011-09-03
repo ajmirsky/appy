994cab42c54159d8e716e407c0cd048f7f4b852c
Andrew Mirsky <andrew@mirsky.net>
2011-09-03 11:03:40 -0400

    setup.py and MANIFEST.in files to create egg and install files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Appy Framework',
+    version='0.6.7',
+    author='Gaetan Delannay',
+    author_email='',
+    packages=find_packages(),
+    scripts=[],
+    url='http://appyframework.org/pod.html',
+    license='LICENSE.txt',
+    description='Basic and advanced classes framework. Security and workflows. Basic and advanced open document templates.',
+    long_description=open('README.txt').read(),
+)
